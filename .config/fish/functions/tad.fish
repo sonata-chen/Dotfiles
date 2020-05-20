0d65cde43830e79c8b50f6e1dc392b2941ef7029
sonata-chen <sonatachen39@gmail.com>
2020-05-20 23:10:08 +0800

    fix bugs

--- .config/fish/functions/tad.fish
+++ .config/fish/functions/tad.fish
@@ -1,5 +1,5 @@
 function tad --description 'Toggle audio playback device between built-in monitor speaker and headphone'
-    set  currentMaster (pacmd list-cards | grep "active profile" | sed 's/\s*active profile: \<//g' | sed 's/\>\s*//g') 
+    set currentMaster (pacmd list-cards | grep "active profile" | sed 's/\s*active profile: <//g'|sed 's/>//g')
 
     if test "$currentMaster" = "output:hdmi-stereo"
         pacmd set-card-profile 0  output:analog-stereo
