3c63a0012582606316022ab2928e4fcca7d46aed
sonata-chen <sonatachen39@gmail.com>
2021-05-13 12:49:32 +0800

    pw-jack

--- scripts/pw-jack-run.sh
+++ scripts/pw-jack-run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ls /usr/local/share/applications/ /usr/share/applications/ $HOME/.local/share/applications/  -1 \
-| sed ' s/\.desktop$//g ; /:$/d ; /^$/d ' | sort -u | bemenu -i | xargs gtk-launch
+| sed ' s/\.desktop$//g ; /:$/d ; /^$/d ' | sort -u | bemenu -i | xargs pw-jack gtk-launch
