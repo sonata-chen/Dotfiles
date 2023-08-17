6f4684c582819738db0953611779ad0326ad6742
sonata-chen <sonatachen39@gmail.com>
2023-08-17 22:57:48 +0800

    update fish config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,45 +1,47 @@
 set fish_greeting
 
-set fish_color_autosuggestion brblack
+# set fish_color_autosuggestion brblack
+set fish_color_autosuggestion white
 set fish_color_cancel red --bold
 set fish_color_command magenta
-set fish_color_comment brblack
+set fish_color_comment white
 set fish_color_cwd green --bold
 set fish_color_cwd_root red
 set fish_color_end red
 set fish_color_error red --bold
 set fish_color_escape brcyan
-set fish_color_host white
+set fish_color_host normal
 set fish_color_host_remote yellow
 set fish_color_match yellow
-set fish_color_normal white
+set fish_color_normal normal
 set fish_color_operator green
 set fish_color_param brblue
 set fish_color_quote yellow
 set fish_color_redirection brgreen
-set fish_color_selection white
+set fish_color_selection normal
 set fish_color_status red
 set fish_color_user yellow
 set fish_color_valid_path --underline
 set fish_color_history_current --bold
-set fish_color_search_match --background=555
+set fish_color_search_match --background=black
 set fish_pager_color_completion green
 set fish_pager_color_description blue
-set fish_pager_color_prefix white --bold --underline
-set fish_pager_color_progress black --bold --background=magenta
-set fish_pager_color_selected_background --background=444
+set fish_pager_color_prefix yellow --bold
+set fish_pager_color_progress cyan --underline --bold
+set fish_pager_color_selected_background --background=black
 
 fish_add_path --path $HOME/scripts
 fish_add_path --path $HOME/.local/bin
 
 eval (dircolors -b ~/.dircolors |sed 2d |string replace "LS_COLORS="  "set -x LS_COLORS ")
 
-set -x BAT_THEME "Solarized (dark)"
+set -x BAT_THEME "TwoDark"
 # set -x BAT_THEME "ansi-dark"
 
 set -x FZF_DEFAULT_OPTS '
-  --color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f
-  --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54'
+  --color fg:-1,bg:-1,hl:3,fg+:-1,bg+:0,hl+:3
+  --color gutter:-1,query:-1,disabled:0
+  --color info:6,prompt:5,spinner:1,pointer:3,marker:1,header:1:bold'
 fzf_key_bindings
 
 # set -x MANPAGER "nvim -c 'set ft=man' -"
