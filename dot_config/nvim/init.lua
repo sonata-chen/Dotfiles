7dd46f567e04afc96fa81c092c3057b51c3b8c4c
sonata-chen <sonatachen39@gmail.com>
2024-02-07 10:16:19 +0800

    update neovim plugins, try out trouble.nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -104,27 +104,11 @@ require('lazy').setup({
 
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
-  { -- Adds git releated signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
+  -- Adds git releated signs to the gutter, as well as utilities for managing changes
+  'lewis6991/gitsigns.nvim',
 
-  { -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
-  },
+  -- Theme inspired by Atom
+  'navarasu/onedark.nvim',
 
   { -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
@@ -144,7 +128,8 @@ require('lazy').setup({
     'lukas-reineke/indent-blankline.nvim',
     main = "ibl",
     opts = {
-      indent = { char = "┊" },
+      -- indent = { char = "┊" },
+      indent = { char = "▏" },
     },
   },
 
--- dot_config/nvim/lua/custom/plugins/null-ls.lua
+++ dot_config/nvim/lua/custom/plugins/null-ls.lua
@@ -1,6 +1,6 @@
 return {
   {
-    'jose-elias-alvarez/null-ls.nvim',
+    'nvimtools/none-ls.nvim',
     config = function (_, _)
       local null_ls = require('null-ls')
       null_ls.setup{
